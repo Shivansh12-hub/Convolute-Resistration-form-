84d7c76a6243d46019a8b37d943c0f9087f77af6
shivansh <shivansh@example.com>
2026-03-16 17:18:29 +0530

    first commit

---
+++ routes/route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { slow } from "../middleware/express-slowDown.js";
+// import { limiter } from "./middleware/rateLimiter.js";
+
+import { resisterStudent, resendOTP,  verifyStudentRegistration, verifyCaptcha } from "../controller/resistration.js";
+
+
+
+const router=Router();
+
+router.route("/register").post(slow,resisterStudent);
+
+router.route("/verify").post( slow,verifyStudentRegistration);
+
+router.route("/resendotp").get(resendOTP);
+router.route("/verifyCaptcha").post(verifyCaptcha);
+
+export default router
